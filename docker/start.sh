dc79d4cf08367b25d102304f61dd1c312590f4f1
Kenneth Borja <k.borja.541047@umindanao.edu.ph>
2026-04-29 00:05:28 +0800

    Fix Render Laravel app key startup

--- docker/start.sh
+++ docker/start.sh
@@ -5,10 +5,6 @@ PORT="${PORT:-10000}"
 sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:[0-9]*>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
-if [ -z "$APP_KEY" ]; then
-    php artisan key:generate --force --no-interaction
-fi
-
 if [ "${DB_CONNECTION:-sqlite}" = "sqlite" ]; then
     export DB_DATABASE="${DB_DATABASE:-/tmp/database.sqlite}"
     touch "$DB_DATABASE"
